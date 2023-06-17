bc76e97fbe4a7d150b3417555534f1af3de77fa1
betocampost <106752128+betocampost@users.noreply.github.com>
2023-06-17 15:02:08 -0800

    Login y register con DB y CSS

---
+++ Login y logout BD CSS/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MainComponent } from './main/main.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    MainComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideMessaging(() => getMessaging()),
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Login y logout BD CSS/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'loginproyecto-66d77',
+    appId: '1:946486542751:web:cb108954f0b3e8e5adbdaa',
+    databaseURL: 'https://loginproyecto-66d77-default-rtdb.firebaseio.com',
+    storageBucket: 'loginproyecto-66d77.appspot.com',
+    apiKey: 'AIzaSyDaLgrqmAn1OhRZ-pjCBIlgQxjyjG63CCQ',
+    authDomain: 'loginproyecto-66d77.firebaseapp.com',
+    messagingSenderId: '946486542751',
+    measurementId: 'G-PRW6QBX559',
+  },
+};
