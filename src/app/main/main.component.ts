52aaefc85510dd9e63704c4803a45375b48225e3
betocampos <bcamposterrones@gmail.com>
2023-06-17 19:21:19 -0600

    corregido main

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+
+  import { Component } from '@angular/core';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
 })
 export class MainComponent {
 
+  constructor(private userService:UserService, private router:Router){}
+
+  onClick() {
+    this.userService
+      .logout()
+      .then(() => {
+        this.router.navigate(['/register']);
+      })
+      .catch((error) => console.log(error));
+  }
 }
+
